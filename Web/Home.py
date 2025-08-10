a17d7bf8b067a97086692baa6a55bc4d933da87e
Arsan Codifire <132670256+ArsanCodifire@users.noreply.github.com>
2025-08-10 18:43:09 +0530

    Update Home.py

--- Web/Home.py
+++ Web/Home.py
@@ -3,9 +3,11 @@ st.set_page_config(
     page_title="Arsan Codifire",
     page_icon=":computer:"
 )
-st.title("Welcome")
-st.write("Hello, I am Arsan Codifire!")
+col1, col2 = st.columns()
+col1.title("Welcome")
+col1.write("Hello, I am Arsan Codifire!")
 st.subheader("Here are some of my projects:")  
 st.write("     1. Base64 Converter")
 st.write("     1. Auto-Clicker")
 st.sidebar.success("Choose a page above")
+
