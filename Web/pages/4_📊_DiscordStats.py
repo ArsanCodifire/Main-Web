bce5f16e6196fec26bc13f25b8c414ce5e643130
Arsan Codifire <arsancodifire@signinid.com>
2025-08-26 22:34:07 +0530

    Update 4_📊_DiscordStats.py

--- Web/pages/4_📊_DiscordStats.py
+++ Web/pages/4_📊_DiscordStats.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import time
 import streamlit.components.v1 as prt
-st.set_page_config(page_title="Discord Stats",page_icon=":robot:") prt.iframe("https://pygamer-b24o.onrender.com", width=350,height=500)
+st.set_page_config(page_title="Discord Stats",page_icon=":robot:")
+prt.iframe("https://pygamer-b24o.onrender.com", width=350,height=500)
 prt.iframe("https://pytools-pm6d.onrender.com", width=350,height=500)
 prt.iframe("https://pymedia.onrender.com", width=350,height=500)
