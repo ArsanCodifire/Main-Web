0a8feed5753c5c575997cb3cef7aa81bc769795b
Arsan Codifire <arsancodifire@signinid.com>
2025-11-21 17:15:01 +0530

    Update Home.py

--- Web/Home.py
+++ Web/Home.py
@@ -6,11 +6,12 @@ st.set_page_config(
 col1, col2 = st.columns(2)
 col1.title("Welcome")
 col1.write("Hello, I am Arsan Codifire!")
-col2.image("https://raw.githubusercontent.com/ArsanCodifire/Main-Web/refs/heads/main/Web/pfp.jpg")
+col2.image("https://raw.githubusercontent.com/ArsanCodifire/Main-Web/main/Web/pfp.jpg")
 st.subheader("Here are some of my projects:")  
 st.write("     1. Base64 Converter")
 st.write("     1. Auto-Clicker")
 st.sidebar.success("Choose a page above")
+
 
 
 
