7e4b2e11d166116cfb38ab63e22f97fe755a5146
Arsan Codifire <arsancodifire@signinid.com>
2025-12-08 18:57:32 +0300

    Update Home.py

--- Web/Home.py
+++ Web/Home.py
@@ -1,8 +1,11 @@
 import streamlit as st
+import os
 st.set_page_config(
     page_title="Arsan Codifire",
     page_icon=":computer:"
 )
+current_folder = os.path.dirname(os.path.abspath(__file__))
+local_image_path = os.path.join(current_folder, "pfp.jpg")
 col1, col2 = st.columns(2)
 col1.title("Welcome")
 col1.write("Hello, I am Arsan Codifire!")
