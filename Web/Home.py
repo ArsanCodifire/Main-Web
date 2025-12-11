37c8c7f0c015333ad39668d48c6632bf847e993f
Arsan Codifire <arsancodifire@signinid.com>
2025-12-11 20:27:34 +0530

    Update Home.py

--- Web/Home.py
+++ Web/Home.py
@@ -12,10 +12,11 @@ col1, col2 = st.columns(2)
 col1.title("Welcome")
 col1.write("Hello, I am Arsan Codifire!")
 if os.path.exists(local_image_path):
-    col2.image(local_image_path, caption="Profile Picture (Local)")
+    col2.image(local_image_path)
 else:
-    col2.image(github_url, caption="Profile Picture (GitHub Backup)")
+    col2.image(github_url)
 st.subheader("Here are some of my projects:")  
 st.write("     1. Base64 Converter")
 st.write("     1. Auto-Clicker")
+
 st.sidebar.success("Choose a page above")
